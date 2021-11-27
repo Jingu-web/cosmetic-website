bc9665fe66f520e11214db6701fe2c78ff18eeea
Jingu-web <multiNomaFeBe.rjrrr@gmail.com>
2021-11-27 13:55:35 +0900

    💄 新着情報一覧のレイアウトの作成

--- js/app.js
+++ js/app.js
@@ -95,3 +95,26 @@ document.querySelector(".hero-section").onmouseover = () => {
 document.querySelector(".hero-section").onmouseleave = () => {
   auto_slide = setInterval(nextSlide, 4000);
 };
+
+document.querySelectorAll(".products-slide").forEach((e) => {
+  new Glide(e.querySelector(".glide"), {
+    type: "slider",
+    starAt: 0,
+    perView: 4,
+    rewind: false,
+    bound: true,
+  }).mount();
+});
+
+document.querySelectorAll(".product-img").forEach((e) => {
+  e.style.width = e.parentElement.offsetWidth + "px";
+  e.style.height = e.parentElement.offsetWidth + "px";
+
+  new hoverEffect({
+    parent: e,
+    intensity: 0.6,
+    image1: e.getAttribute("data-img-1"),
+    image2: e.getAttribute("data-img-2"),
+    displacementImage: "images/distortion.jpg",
+  });
+});
